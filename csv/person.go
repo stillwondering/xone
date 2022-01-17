ee1d6e3070ebd09331818ec7dd577c53fc5d66db
stillwondering <44878817+stillwondering@users.noreply.github.com>
2022-01-17 10:02:46 +0100

    Enable parsing/writing to file

--- csv/person.go
+++ csv/person.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"os"
 	"time"
 
 	"github.com/stillwondering/xone"
@@ -31,6 +32,16 @@ func Write(dst io.Writer, persons []xone.Person) error {
 	_, err := io.Copy(dst, &buf)
 
 	return err
+}
+
+func WriteFile(file string, persons []xone.Person) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return Write(f, persons)
 }
 
 func Parse(src io.Reader) ([]xone.Person, error) {
@@ -61,6 +72,16 @@ func Parse(src io.Reader) ([]xone.Person, error) {
 	}
 
 	return persons, nil
+}
+
+func ParseFile(file string) ([]xone.Person, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return Parse(f)
 }
 
 func getDefaultReader(src io.Reader) *csv.Reader {
--- csv/person_test.go
+++ csv/person_test.go
@@ -3,6 +3,8 @@ package csv
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
+	"path"
 	"reflect"
 	"strings"
 	"testing"
@@ -61,6 +63,55 @@ func TestWrite(t *testing.T) {
 			}
 			if gotDst := dst.String(); gotDst != tt.wantDst {
 				t.Errorf("Write() = %v, want %v", gotDst, tt.wantDst)
+			}
+		})
+	}
+}
+
+func TestWriteFile(t *testing.T) {
+	dir := t.TempDir()
+
+	type args struct {
+		file    string
+		persons []xone.Person
+	}
+	tests := []struct {
+		name            string
+		args            args
+		compareWithFile string
+		wantErr         bool
+	}{
+		{
+			name: "EmptyFile",
+			args: args{
+				file:    path.Join(dir, "EmptyFile.csv"),
+				persons: []xone.Person{},
+			},
+			compareWithFile: "testdata/EmptyFile.csv",
+			wantErr:         false,
+		},
+		{
+			name: "MultiplePeople",
+			args: args{
+				file: path.Join(dir, "MultiplePeople.csv"),
+				persons: []xone.Person{
+					{FirstName: "Harry", LastName: "Potter", DateOfBirth: dateFromString(t, "1980-07-31")},
+					{FirstName: "Ron", LastName: "Weasley", DateOfBirth: dateFromString(t, "1980-03-01")},
+					{FirstName: "Hermione", LastName: "Granger", DateOfBirth: dateFromString(t, "1979-09-19")},
+				},
+			},
+			compareWithFile: "testdata/MultiplePeople.csv",
+			wantErr:         false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := WriteFile(tt.args.file, tt.args.persons); (err != nil) != tt.wantErr {
+				t.Errorf("WriteFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if !areContentsEqual(t, tt.compareWithFile, tt.args.file) {
+				t.Errorf("WriteFile() does not match %s", tt.compareWithFile)
 			}
 		})
 	}
@@ -137,6 +188,53 @@ func TestParse(t *testing.T) {
 	}
 }
 
+func TestParseFile(t *testing.T) {
+	type args struct {
+		file string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []xone.Person
+		wantErr bool
+	}{
+		{
+			name:    "IncorrectNumberOfColumns",
+			args:    args{file: "testdata/IncorrectNumberOfColumns.csv"},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "EmptyFile",
+			args:    args{file: "testdata/EmptyFile.csv"},
+			want:    nil,
+			wantErr: false,
+		},
+		{
+			name: "MultiplePeople",
+			args: args{file: "testdata/MultiplePeople.csv"},
+			want: []xone.Person{
+				{FirstName: "Harry", LastName: "Potter", DateOfBirth: dateFromString(t, "1980-07-31")},
+				{FirstName: "Ron", LastName: "Weasley", DateOfBirth: dateFromString(t, "1980-03-01")},
+				{FirstName: "Hermione", LastName: "Granger", DateOfBirth: dateFromString(t, "1979-09-19")},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseFile(tt.args.file)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func dateFromString(t *testing.T, s string) time.Time {
 	t.Helper()
 	d, err := time.Parse(xone.FormatDateOfBirth, s)
@@ -145,4 +243,20 @@ func dateFromString(t *testing.T, s string) time.Time {
 	}
 
 	return d
+}
+
+func areContentsEqual(t *testing.T, expected, actual string) bool {
+	t.Helper()
+
+	expectedContent, err := ioutil.ReadFile(expected)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actualContent, err := ioutil.ReadFile(actual)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return bytes.Equal(expectedContent, actualContent)
 }
