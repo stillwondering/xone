4b520e24f0d72f5f919f4ca9b113532c5fe1ee31
stillwondering <44878817+stillwondering@users.noreply.github.com>
2022-01-27 18:52:10 +0100

    Add individual person ID

--- sqlite/migration/00000000.sql
+++ sqlite/migration/00000000.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `person` (
     `id` INTEGER NOT NULL PRIMARY KEY,
+    `pid` TEXT NOT NULL UNIQUE,
     `first_name` TEXT NOT NULL,
     `last_name` TEXT NOT NULL,
     `date_of_birth` TEXT NOT NULL,
