a9be6f93346ef28d3e3492474da4bce95ca3f958
stillwondering <44878817+stillwondering@users.noreply.github.com>
2022-01-29 10:40:34 +0100

    Remove unused code

--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -1,15 +1,10 @@
 package sqlite_test
 
 import (
-	"flag"
-	"io/ioutil"
-	"path/filepath"
 	"testing"
 
 	"github.com/stillwondering/xone/sqlite"
 )
-
-var dump = flag.Bool("dump", false, "save work data")
 
 // Ensure the test database can open & close.
 func TestDB(t *testing.T) {
@@ -21,19 +16,7 @@ func TestDB(t *testing.T) {
 func MustOpenDB(tb testing.TB) *sqlite.DB {
 	tb.Helper()
 
-	// Write to an in-memory database by default.
-	// If the -dump flag is set, generate a temp file for the database.
-	dsn := ":memory:"
-	if *dump {
-		dir, err := ioutil.TempDir("", "")
-		if err != nil {
-			tb.Fatal(err)
-		}
-		dsn = filepath.Join(dir, "db")
-		println("DUMP=" + dsn)
-	}
-
-	db := sqlite.NewDB(dsn)
+	db := sqlite.NewDB(":memory:")
 	if err := db.Open(); err != nil {
 		tb.Fatal(err)
 	}
