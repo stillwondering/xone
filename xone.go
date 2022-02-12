6b8be437e507f53b407af1e42cdff3a700d5ab56
stillwondering <44878817+stillwondering@users.noreply.github.com>
2022-02-12 13:01:05 +0100

    Enable calculating a person's age

--- xone.go
+++ xone.go
@@ -42,6 +42,37 @@ type Person struct {
 	Gender      Gender
 }
 
+// Age calculates a person's age based on their date of birth and with respect
+// to the given date.
+func (p Person) Age(today time.Time) int {
+	if !p.HasDateOfBirth() {
+		return 0
+	}
+
+	today = today.In(p.DateOfBirth.Location())
+	ty, tm, td := today.Date()
+	today = time.Date(ty, tm, td, 0, 0, 0, 0, time.UTC)
+
+	by, bm, bd := p.DateOfBirth.Date()
+	dob := time.Date(by, bm, bd, 0, 0, 0, 0, time.UTC)
+
+	if today.Before(dob) {
+		return 0
+	}
+
+	age := ty - by
+	anniversary := dob.AddDate(age, 0, 0)
+	if anniversary.After(today) {
+		age--
+	}
+
+	return age
+}
+
+func (p Person) HasDateOfBirth() bool {
+	return !p.DateOfBirth.IsZero()
+}
+
 // CreatePersonData contains all data which is necessary to create a new Person entry
 // in any kind of repository.
 type CreatePersonData struct {
---
+++ xone_test.go
@@ -0,0 +1,80 @@
+package xone
+
+import (
+	"testing"
+	"time"
+)
+
+func TestPerson_Age(t *testing.T) {
+	type fields struct {
+		ID          int
+		FirstName   string
+		LastName    string
+		DateOfBirth time.Time
+		Gender      Gender
+	}
+	type args struct {
+		today time.Time
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "Today before date of birth",
+			fields: fields{
+				DateOfBirth: time.Date(1980, time.July, 31, 0, 0, 0, 0, time.UTC),
+			},
+			args: args{
+				today: time.Date(1980, time.July, 30, 0, 0, 0, 0, time.UTC),
+			},
+			want: 0,
+		},
+		{
+			name: "Birthday is today",
+			fields: fields{
+				DateOfBirth: time.Date(1980, time.July, 31, 0, 0, 0, 0, time.UTC),
+			},
+			args: args{
+				today: time.Date(2000, time.July, 31, 0, 0, 0, 0, time.UTC),
+			},
+			want: 20,
+		},
+		{
+			name: "Today is after a birthday",
+			fields: fields{
+				DateOfBirth: time.Date(1980, time.July, 31, 0, 0, 0, 0, time.UTC),
+			},
+			args: args{
+				today: time.Date(2000, time.August, 1, 0, 0, 0, 0, time.UTC),
+			},
+			want: 20,
+		},
+		{
+			name: "Date of birth is missing",
+			fields: fields{
+				DateOfBirth: time.Time{},
+			},
+			args: args{
+				today: time.Now(),
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Person{
+				ID:          tt.fields.ID,
+				FirstName:   tt.fields.FirstName,
+				LastName:    tt.fields.LastName,
+				DateOfBirth: tt.fields.DateOfBirth,
+				Gender:      tt.fields.Gender,
+			}
+			if got := p.Age(tt.args.today); got != tt.want {
+				t.Errorf("Person.Age() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
