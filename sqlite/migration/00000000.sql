1c0260b97a0c9d99e41dc6f5bfc4135e6548a6ee
stillwondering <44878817+stillwondering@users.noreply.github.com>
2022-02-14 09:08:18 +0100

    Enable tracking history of person table

--- sqlite/migration/00000000.sql
+++ sqlite/migration/00000000.sql
@@ -6,6 +6,56 @@ CREATE TABLE `person` (
     `date_of_birth` TEXT NOT NULL
 );
 
+CREATE TABLE `person_history` (
+    `id` INTEGER NOT NULL PRIMARY KEY,
+    `created_at` TEXT NOT NULL,
+    `person_id` INTEGER REFERENCES `person`(`id`) ON DELETE CASCADE,
+    `public_id` TEXT NOT NULL,
+    `first_name` TEXT NOT NULL,
+    `last_name` TEXT NOT NULL,
+    `date_of_birth` TEXT NOT NULL
+);
+
+CREATE TRIGGER update_history_after_insert_person
+    AFTER INSERT ON person
+BEGIN
+    INSERT INTO person_history (
+        created_at,
+        person_id,
+        public_id,
+        first_name,
+        last_name,
+        date_of_birth
+    ) VALUES (
+        datetime(),
+        NEW.id,
+        NEW.public_id,
+        NEW.first_name,
+        NEW.last_name,
+        NEW.date_of_birth
+    );
+END;
+
+CREATE TRIGGER update_history_after_update_person
+    AFTER UPDATE ON person
+BEGIN
+    INSERT INTO person_history (
+        created_at,
+        person_id,
+        public_id,
+        first_name,
+        last_name,
+        date_of_birth
+    ) VALUES (
+        datetime(),
+        NEW.id,
+        NEW.public_id,
+        NEW.first_name,
+        NEW.last_name,
+        NEW.date_of_birth
+    );
+END;
+
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL PRIMARY KEY,
     `email` TEXT NOT NULL UNIQUE,
